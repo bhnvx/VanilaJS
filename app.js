5b3656cc030b86784ae3b7873d8a8491c7f709f6
bhnvx <xzt______@naver.com>
2022-10-11 10:58:23 +0900

    function TEST

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
-const InputText = document.querySelector("#input-form input");
-const ButtonForm = document.querySelector("#input-form button");
+const loginForm = document.querySelector("#input-form");
+const loginInput = document.querySelector("#input-form input");
+const greeting = document.querySelector('#greeting');
 
-function onInputBtnCLick() {
-  console.log("Click");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreeting(username);
 }
 
-ButtonForm.addEventListener("click", onInputBtnCLick);
+function paintGreeting(username) {
+  greeting.innerText = `Hello! ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreeting(savedUsername);
+}
